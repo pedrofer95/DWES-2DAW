860a7a10cea121b4d1715df8d85207b3ddcd6ee8
pedro.fernandez2 <71818355+pedrofer95@users.noreply.github.com>
2020-09-28 10:55:41 +0200

    Add files via upload

---
+++ ej2.php
@@ -0,0 +1,16 @@
+<html>
+<head> <meta charset="utf-8"/> </head>
+<body>
+    <?php
+        echo "Escribiremos el número 34 en binario, en octal y en hexadecimal <br><br>";
+        $num = 34;
+
+        printf ("En binario se representa como %b",$num);
+        echo "<br>";
+        printf ("En octal se representa como %o",$num);
+        echo "<br>";
+        printf ("El hexadecimal se representa como %x",$num);
+    ?>
+</body>
+
+</html>
