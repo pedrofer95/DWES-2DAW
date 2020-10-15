35b948030c5d5ae5dac5faa736e80da8695e5e28
Alfonso Rebolleda Sánchez <47109499+arebolleda@users.noreply.github.com>
2020-10-15 09:45:10 +0200

    Update ej2a.php

--- PHP/ej2a.php
+++ PHP/ej2a.php
@@ -6,7 +6,7 @@
     <body>
         <?php 
             // PEDRO FERNÁNDEZ GARCíA
-
+/*ALFONSO:<<<< Los arrays tienen que generarse de manera dinámica con bucles */
             $impares=array(1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39);
             $suma = 0;
             $arrlength = count($impares);
